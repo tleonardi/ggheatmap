d4c72f0c8fdc1292e1e40130ff1e9e4d9f07a6d2
Tommaso Leonardi <tom@itm6.xyz>
2018-04-09 19:10:40 +0100

    More code tidying up

--- R/ggheatmap.R
+++ R/ggheatmap.R
@@ -18,6 +18,7 @@ ggheatmap <- function(dataMatrix, orderCol = T, orderRow = T, dendroLineSize = 0
     
     data_m <- tibble::rownames_to_column(dataMatrix) %>% reshape2::melt()
     
+    # Cluster rows
     if (orderRow) {
         dd.row <- as.dendrogram(hclust(dist(dataMatrix, method = distMethod), method = clustMethod))
         row.ord <- order.dendrogram(dd.row)
@@ -28,36 +29,41 @@ ggheatmap <- function(dataMatrix, orderCol = T, orderRow = T, dendroLineSize = 0
     # Cluster columns
     if (orderCol) {
         dd.col <- as.dendrogram(hclust(dist(t(dataMatrix), method = distMethod), 
-            method = clustMethod))
+                      method = clustMethod))
         col.ord <- order.dendrogram(dd.col)
         ordered_col_names <- colnames(dataMatrix[, col.ord])
         data_m$variable <- factor(data_m$variable, levels = ordered_col_names)
     }
     
     heat_plot <- ggplot2::ggplot(data_m, aes(x = variable, y = rowname, fill = value)) + 
-        ggplot2::geom_tile() + ggplot2::theme_minimal() + ggplot2::theme(axis.line = element_line(size = 0), 
-        text = element_text(size = fontSize)) + ggplot2::scale_y_discrete(position = "right") + 
-        ggplot2::xlab("") + ggplot2::ylab("") + ggplot2::scale_fill_distiller(palette = colorPalette, 
-        name = scaleName)
+                     ggplot2::geom_tile() + 
+		     ggplot2::theme_minimal() + 
+		     ggplot2::theme(axis.line = element_line(size = 0),
+		         text = element_text(size = fontSize)) + 
+                     ggplot2::scale_y_discrete(position = "right") + 
+                     ggplot2::xlab("") + 
+		     ggplot2::ylab("") + 
+		     ggplot2::scale_fill_distiller(palette = colorPalette, name = scaleName)
     
     final_plot <- heat_plot
-    # Cluster rows
+
     if (orderRow) {
         dendro_data_row <- ggdendro::dendro_data(dd.row, type = "rectangle")
         dendro_row <- cowplot::axis_canvas(heat_plot, axis = "y", coord_flip = TRUE) + 
-            ggplot2::geom_segment(data = segment(dendro_data_row), aes(y = -y, x = x, 
-                xend = xend, yend = -yend), size = dendroLineSize) + ggplot2::coord_flip()
+                          ggplot2::geom_segment(data = segment(dendro_data_row), aes(y = -y, 
+	                  x = x, xend = xend, yend = -yend), size = dendroLineSize) + 
+                          ggplot2::coord_flip()
         final_plot <- cowplot::insert_yaxis_grob(final_plot, dendro_row, grid::unit(0.2, 
-            "null"), position = "left")
+                          "null"), position = "left")
     }
     
-    # Cluster columns
     if (orderCol) {
         dendro_data_col <- ggdendro::dendro_data(dd.col, type = "rectangle")
-        dendro_col <- cowplot::axis_canvas(heat_plot, axis = "x") + ggplot2::geom_segment(data = segment(dendro_data_col), 
-            aes(x = x, y = y, xend = xend, yend = yend), size = dendroLineSize)
+        dendro_col <- cowplot::axis_canvas(heat_plot, axis = "x") + 
+		      ggplot2::geom_segment(data = segment(dendro_data_col), 
+                          aes(x = x, y = y, xend = xend, yend = yend), size = dendroLineSize)
         final_plot <- cowplot::insert_xaxis_grob(final_plot, dendro_col, grid::unit(0.2, 
-            "null"), position = "top")
+                          "null"), position = "top")
     }
     
     cowplot::ggdraw(final_plot)
