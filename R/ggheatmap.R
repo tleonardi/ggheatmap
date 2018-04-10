eba455acb238e09a971ce2324b0256881e6e398e
Tommaso Leonardi <tom@itm6.xyz>
2018-04-10 11:47:39 +0100

    Fixed missing namespace

--- R/ggheatmap.R
+++ R/ggheatmap.R
@@ -13,7 +13,7 @@
 #' @importFrom magrittr %>%
 #' @export 
 ggheatmap <- function(dataMatrix, orderCol = T, orderRow = T, dendroLineSize = 0.5, 
-    fontSize = 20, colorScheme = "Spectral", scaleName = "value", distMethod = "euclidean", 
+    fontSize = 20, colorPalette = "Spectral", scaleName = "value", distMethod = "euclidean", 
     clustMethod = "complete") {
     
     data_m <- tibble::rownames_to_column(dataMatrix) %>% reshape2::melt()
@@ -38,8 +38,8 @@ ggheatmap <- function(dataMatrix, orderCol = T, orderRow = T, dendroLineSize = 0
     heat_plot <- ggplot2::ggplot(data_m, ggplot2::aes(x = variable, y = rowname, fill = value)) + 
                      ggplot2::geom_tile() + 
 		     ggplot2::theme_minimal() + 
-		     ggplot2::theme(axis.line = element_line(size = 0),
-		         text = element_text(size = fontSize)) + 
+		     ggplot2::theme(axis.line = ggplot2::element_line(size = 0),
+		         text = ggplot2::element_text(size = fontSize)) + 
                      ggplot2::scale_y_discrete(position = "right") + 
                      ggplot2::xlab("") + 
 		     ggplot2::ylab("") + 
@@ -50,7 +50,7 @@ ggheatmap <- function(dataMatrix, orderCol = T, orderRow = T, dendroLineSize = 0
     if (orderRow) {
         dendro_data_row <- ggdendro::dendro_data(dd.row, type = "rectangle")
         dendro_row <- cowplot::axis_canvas(heat_plot, axis = "y", coord_flip = TRUE) + 
-                          ggplot2::geom_segment(data = segment(dendro_data_row), ggplot2::aes(y = -y, 
+                          ggplot2::geom_segment(data = ggdendro::segment(dendro_data_row), ggplot2::aes(y = -y, 
 	                  x = x, xend = xend, yend = -yend), size = dendroLineSize) + 
                           ggplot2::coord_flip()
         final_plot <- cowplot::insert_yaxis_grob(final_plot, dendro_row, grid::unit(0.2, 
@@ -60,7 +60,7 @@ ggheatmap <- function(dataMatrix, orderCol = T, orderRow = T, dendroLineSize = 0
     if (orderCol) {
         dendro_data_col <- ggdendro::dendro_data(dd.col, type = "rectangle")
         dendro_col <- cowplot::axis_canvas(heat_plot, axis = "x") + 
-		      ggplot2::geom_segment(data = segment(dendro_data_col), ggplot2::aes(x = x, y = y, xend = xend, yend = yend), size = dendroLineSize)
+		      ggplot2::geom_segment(data = ggdendro::segment(dendro_data_col), ggplot2::aes(x = x, y = y, xend = xend, yend = yend), size = dendroLineSize)
         final_plot <- cowplot::insert_xaxis_grob(final_plot, dendro_col, grid::unit(0.2, 
                           "null"), position = "top")
     }
